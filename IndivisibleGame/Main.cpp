504509763f5eab781be864252d96cc61710b5bd6
Sean H Fagan <seanfagandesign@gmail.com>
2018-11-14 22:33:02 -0600

    Parse comma delimited text

--- IndivisibleGame/Main.cpp
+++ IndivisibleGame/Main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
+#include <sstream>
+#include <vector>
 
 int main()
 {
 	std::cout << "Hi world." << std::endl;
+
+	std::stringstream ss("1a,2a, 3b, a1, aa ,");
+	std::vector<std::string> result;
+
+	while (ss.good())
+	{
+		std::string substr;
+		std::getline(ss, substr, ',');
+		result.push_back(substr);
+	}
+
+	std::cout << "Beginning" << std::endl;
+	for (auto i : result)
+	{
+		std::cout << "|" << i << "|" << std::endl;
+	}
+	std::cout << "End" << std::endl;
+
 	std::cin.get();
 }
