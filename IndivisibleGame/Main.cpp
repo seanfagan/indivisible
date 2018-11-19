3fe0ac529d03d5db833e811f566dfecad78c6e67
Sean H Fagan <seanfagandesign@gmail.com>
2018-11-19 00:57:26 -0600

    Add functions for parsing user selections

--- IndivisibleGame/Main.cpp
+++ IndivisibleGame/Main.cpp
@@ -1,27 +1,70 @@
+#include <algorithm>
+#include <cctype>
 #include <iostream>
 #include <sstream>
+#include <string>
 #include <vector>
+
+
+bool is_invalid_notation(char ch)
+{
+	/**
+		Checks if a char is invalid for space notation.
+
+		@param ch The char to validate.
+		@return A boolean of if the char is invalid.
+	*/
+	return not std::isalnum(static_cast<unsigned char>(ch));
+}
+
+
+std::vector<std::string> parse_selection(std::string input)
+{
+	/**
+		Parses the user's input for selecting spaces.
+
+		@param input The user's input to parse.
+		@return A vector of all selections the user input.
+	*/
+	std::stringstream ss(input); // convert input to stringstream
+	std::vector<std::string> result;
+
+	// loop through input
+	while (ss.good())
+	{
+		std::string substr; // for each substring
+		std::getline(ss, substr, ','); // comma delimiter
+
+		// remove invalid characters
+		substr.erase(std::remove_if(substr.begin(), substr.end(), (int(*)(int))is_invalid_notation), substr.end());
+
+		// convert to uppercase
+		std::transform(substr.begin(), substr.end(), substr.begin(), ::toupper);
+
+		result.push_back(substr);
+	}
+
+	return result;
+}
+
 
 int main()
 {
 	std::cout << "Hi world." << std::endl;
 
-	std::stringstream ss("1a,2a, 3b, a1, aa ,");
-	std::vector<std::string> result;
+	// get input
+	std::string input;
+	std::getline(std::cin, input);
 
-	while (ss.good())
-	{
-		std::string substr;
-		std::getline(ss, substr, ',');
-		result.push_back(substr);
-	}
+	// parse input into vector
+	std::vector<std::string> result = parse_selection(input);
 
-	std::cout << "Beginning" << std::endl;
+	// print vector
 	for (auto i : result)
 	{
 		std::cout << "|" << i << "|" << std::endl;
 	}
-	std::cout << "End" << std::endl;
 
+	std::cout << "End" << std::endl;
 	std::cin.get();
 }
