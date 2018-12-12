20bef40207d2e0edc8ceef42bc694bbf7f386e6f
Sean H Fagan <seanfagandesign@gmail.com>
2018-12-11 20:15:57 -0600

    Add regex check to the user's input (plus some tweaks)

--- IndivisibleGame/Main.cpp
+++ IndivisibleGame/Main.cpp
@@ -1,15 +1,16 @@
 #include <algorithm>
 #include <cctype>
 #include <iostream>
+#include <regex>
 #include <sstream>
 #include <string>
 #include <vector>
 
 
-bool is_invalid_notation(char ch)
+bool is_invalid_char(char ch)
 {
 	/**
-		Checks if a char is invalid for space notation.
+		Checks if a char is invalid for tile notation.
 
 		@param ch The char to validate.
 		@return A boolean of if the char is invalid.
@@ -17,31 +18,44 @@ bool is_invalid_notation(char ch)
 	return not std::isalnum(static_cast<unsigned char>(ch));
 }
 
+bool is_valid_notation(std::string selection)
+{
+	std::regex rgx("^[A-Z]\d$");
+
+	return std::regex_match(selection, rgx);
+} 
+
 
 std::vector<std::string> parse_selection(std::string input)
 {
 	/**
-		Parses the user's input for selecting spaces.
+		Parses the user's input for selecting tiles. Does some cleaning and validating.
 
 		@param input The user's input to parse.
-		@return A vector of all selections the user input.
+		@return A vector of all the user's tile selections.
 	*/
 	std::stringstream ss(input); // convert input to stringstream
 	std::vector<std::string> result;
+	std::regex rgx("[A-Z]\\d"); // regex of valid notation
 
-	// loop through input
 	while (ss.good())
 	{
-		std::string substr; // for each substring
+		// separate a substring
+		std::string substr;
 		std::getline(ss, substr, ','); // comma delimiter
 
-		// remove invalid characters
-		substr.erase(std::remove_if(substr.begin(), substr.end(), (int(*)(int))is_invalid_notation), substr.end());
+		// strip out invalid characters
+		substr.erase(std::remove_if(substr.begin(), substr.end(), is_invalid_char), substr.end());
 
 		// convert to uppercase
 		std::transform(substr.begin(), substr.end(), substr.begin(), ::toupper);
 
-		result.push_back(substr);
+		// run against regex
+		if (std::regex_match(substr, rgx)) {
+			result.push_back(substr);
+		} else {
+			std::cout << "I don't understand '" << substr << "'. Selections must be '[Letter][Number]', like 'A1'." << std::endl;
+		};
 	}
 
 	return result;
