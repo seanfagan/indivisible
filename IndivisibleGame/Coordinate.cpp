fc69ea85cf3643789de75c8dc052325a434edda4
Sean H Fagan <seanfagandesign@gmail.com>
2019-12-16 21:31:45 -0600

    Cleanup of Coordinate

--- IndivisibleGame/Coordinate.cpp
+++ IndivisibleGame/Coordinate.cpp
@@ -1,21 +1,34 @@
 #include "Coordinate.h"
 #include <tuple>
 
-// operators
+// Constructors
+Coordinate::Coordinate()
+	: x(0), y(0) {}
+
+Coordinate::Coordinate(int const& x, int const& y)
+	: x(x), y(y) {}
+
+Coordinate::Coordinate(std::string const& notation) {
+	std::tuple<int, int> xy = to_cartesian(notation);
+	x = std::get<0>(xy);
+	y = std::get<1>(xy);
+}
+
+// Operators
 std::ostream& operator<<(std::ostream& os, Coordinate& d)
 {
 	os << d.to_notation(d.x, d.y);
 	return os;
 }
 
-std::ostream& operator<<(std::ostream& os, const Coordinate& d)
+std::ostream& operator<<(std::ostream& os, Coordinate const& d)
 {
 	os << d.to_notation(d.x, d.y);
 	return os;
 }
 
-// static
-std::tuple<int, int> Coordinate::to_cartesian(const std::string& notation) {
+// Static methods
+std::tuple<int, int> Coordinate::to_cartesian(std::string const& notation) {
 	/** Convert coordinates from board notation ("A1") to cartesian (0, 0). */
 	int a = notation[0] - 'A';
 	int b = notation[1] - '1';
@@ -23,24 +36,10 @@ std::tuple<int, int> Coordinate::to_cartesian(const std::string& notation) {
 	return std::tuple<int, int>(a, b);
 }
 
-std::string Coordinate::to_notation(const int& x, const int& y) {
+std::string Coordinate::to_notation(int const& x, int const& y) {
 	/** Convert coordinates from cartesian (0, 0) to board notation ("A1"). */
 	char a = 'A' + x;
 	char b = '1' + y;
 
 	return std::string{ a, b };
 }
-
-// nonstatic
-Coordinate::Coordinate()
-	: x(0), y(0) {}
-
-Coordinate::Coordinate(const int& x, const int& y)
-	: x(x), y(y) {}
-
-Coordinate::Coordinate(const std::string& notation) {
-	std::tuple<int, int> xy = to_cartesian(notation);
-	x = std::get<0>(xy);
-	y = std::get<1>(xy);
-}
-
--- IndivisibleGame/Coordinate.h
+++ IndivisibleGame/Coordinate.h
@@ -1,17 +1,20 @@
 #pragma once
 #include <string>
 
+
 struct Coordinate {
 	int x, y;
 
+	// Constructors
 	Coordinate();
-	Coordinate(const int& x, const int& y);
-	Coordinate(const std::string& notation);
+	Coordinate(int const& x, int const& y);
+	Coordinate(std::string const& notation);
 
-	// operator
+	// Operators
 	friend std::ostream& operator<<(std::ostream& os, Coordinate& d);
-	friend std::ostream& operator<<(std::ostream& os, const Coordinate& d);
+	friend std::ostream& operator<<(std::ostream& os, Coordinate const& d);
 
-	static std::tuple<int, int> to_cartesian(const std::string& notation);
-	static std::string to_notation(const int& x, const int& y);
+	// Static methods
+	static std::tuple<int, int> to_cartesian(std::string const& notation);
+	static std::string to_notation(int const& x, int const& y);
 };
